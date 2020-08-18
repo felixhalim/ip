7b9733f57298dd44132feb47d66596819cd1a28f
felixhalim <felixh@comp.nus.edu.sg>
2020-08-18 23:05:08 +0800

    Add printGreetings method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
 public class Duke {
+    private static void printGreetings() {
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printGreetings();
     }
 }
