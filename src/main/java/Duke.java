58d38072eb2148962c86c33bf995d96025808327
felixhalim <felixh@comp.nus.edu.sg>
2020-08-24 22:36:20 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printFarewells() {
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
     private static void printGreetings() {
+        printHorizontalLine();
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
@@ -13,5 +26,15 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         printGreetings();
+        String userResponse;
+        Scanner in = new Scanner(System.in);
+        do {
+            userResponse = in.nextLine();
+            if (userResponse.equals("bye")) continue;
+            printHorizontalLine();
+            System.out.println(userResponse);
+            printHorizontalLine();
+        } while (!userResponse.equals("bye"));
+        printFarewells();
     }
 }
